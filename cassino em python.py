8112be7fd09d8e538d19b9a6ac6339fdc71a22f0
PedroTesla <105446915+UIy2KHDkF6WPXx3k83Df8w@users.noreply.github.com>
2022-06-29 21:44:45 -0300

    Add files via upload

---
+++ cassino em python.py
@@ -0,0 +1,16 @@
+print("Bem vindo")
+print("logo avisando que esse cassino é básico")
+print("[a] começar")
+print("nome de user no telegram: pedro_py")
+#começo
+print("")
+r = input("")
+#a
+if r == "a":
+    resposta = input("coloque seu nome de usuário:")
+print("bem vindo", (resposta))
+print("")
+import random
+print("seu jogo foi:")
+resposta = ["177", "771", "717", "171", "711", "117", "117", "177", "771", "777", "111"]
+print (random.choice(resposta))
